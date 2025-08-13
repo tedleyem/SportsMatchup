e7abc26907b11df4740fb402df034117616579c9
Tedley Meralus <tmeralus@gmail.com>
2025-08-13 17:52:27 -0400

    added host: true to allow access outside of docker container

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    port: 3000, // Ensure Vite's dev server uses port 3000 to match EXPOSE
+    host: true // Allow access from outside the container
+  },
 });
