657108922c2b0e514043b501b57a374281cab6cc
Tedley Meralus <tmeralus@gmail.com>
2025-08-12 22:49:54 -0400

    updated backend, added blue pill

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,116 @@
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, render_template_string, request
 app =Flask(__name__)
+
+# ASCII art of Neo
+neo_art = """
+⠀⠀⠀⠀⠀⠀⢀⣤⣶⣶⣶⣶⣦⣤⣀⠀⠀⠀⠀⠀
+⠀⠀⢀⣤⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀
+⠀⢠⣿⣿⣿⣿⣿⠿⣿⣿⣿⣿⠿⠿⢿⣿⣿⣷⡀⠀
+⠀⢸⣿⡿⠋⠁⠀⠀⠀⠉⠉⠀⠀⠀⠀⠈⢹⣿⡇⠀
+⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀
+⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀
+⠀⢸⣿⣠⣤⣶⣶⣶⣦⣀⣀⣴⣶⣶⣶⣤⣄⣿⡇⡀
+⣿⣿⣿⠻⣿⣿⣿⣿⣿⠟⠻⣿⣿⣿⣿⣿⠟⣿⣿⣿
+⣿⣿⣿⠀⠈⠉⠛⠋⠉⠀⠀⠉⠙⠛⠉⠁⠀⣿⣿⣿
+⠙⢿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡿⠃
+⠀⠸⣿⣧⠀⠀⠀⢀⣀⣀⣀⣀⡀⠀⠀⠀⣼⣿⠇⠀
+⠀⠀⠙⢿⣷⣄⠀⠈⠉⠉⠉⠉⠁⠀⣠⣾⡿⠃⠀⠀
+⠀⠀⠀⢸⣿⣿⣷⣤⣀⣀⣀⣀⣤⣾⣿⣿⡅⠀⠀⠀
+⠀⠀⢰⣿⣿⣿⣿⣿⣿⡿⠿⢿⣿⣿⣿⣿⣿⡄⠀⠀
+⠀⠀⠻⠿⠿⠿⠿⠿⠿⠷⠴⠿⠿⠿⠿⠿⠿⠇⠀⠀
+"""
+
+# Basic HTML template for the main page
+template = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Neo Matrix</title>
+    <style>
+        body {
+            background-color: black;
+            color: #00FF00;
+            font-family: 'Courier New', monospace;
+            text-align: center;
+            margin-top: 50px;
+        }
+        pre {
+            display: inline-block;
+            text-align: left;
+        }
+        .links {
+            margin-top: 20px;
+        }
+        .links a {
+            color: #00FF00;
+            margin: 0 15px;
+            text-decoration: none;
+            font-size: 18px;
+        }
+        .links a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <pre>{{ neo_art }}</pre>
+    <p>Just take the blue pill</p>
+    <div class="links">
+        <a href="/api/test">API TEST</a>
+        <a href="/api/teams">API TEAMS</a>
+        <a href="/api/data">RAPID API TEST</a>
+    </div>
+</body>
+</html>
+"""
+
+
+@app.route('/')
+def index():
+    return render_template_string(template, neo_art=neo_art)
+
+
+@app.route('/api/data', methods=['GET'])
+def get_rapidapi_data():
+    try:
+        # Get query parameters from the request (optional)
+        query_param = request.args.get('param', '')
+
+        # Set up headers for RapidAPI
+        headers = {
+            'X-RapidAPI-Key': RAPID_API_KEY,
+            'X-RapidAPI-Host': RAPID_API_HOST
+        }
+
+        # Optional: Add query parameters for the RapidAPI request
+        params = {
+            'param': query_param  # Adjust based on the API's required parameters
+        }
+
+        # Make the request to RapidAPI
+        response = requests.get(RAPID_API_URL, headers=headers, params=params)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            data = response.json()
+            return jsonify({
+                'status': 'success',
+                'data': data
+            }), 200
+        else:
+            return jsonify({
+                'status': 'error',
+                'message': f'RapidAPI request failed with status {response.status_code}',
+                'details': response.text
+            }), response.status_code
+
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': 'An error occurred',
+            'details': str(e)
+        }), 500
+        
 
 @app.route("/api/test")
 def test():
